17846b9e77bf32d57dee64bbce26fa1dc5b240c9
mljli <liminglangjun@gmail.com>
2016-09-11 14:33:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# RubySpan
+
+#### Min SDK 21 (Android 5.0 Lollipop)
+
+RubySpan is an attempt to display [ruby annotation](https://www.w3.org/TR/ruby/) along with text using Android TextView. Compared to the WebView solution, using RubySpan with TextView is a lot faster, flexible and more friendly to layouts. Current implementation focuses on displaying furigana on Japanese kanji. But you can easilly adapt it to your application. The minimal API supported is API 21 (Android 5.0 Jelly Bean). Since layouting text is a complex thing, there're still some bugs and the result is not perfect if you compare it with that produced by the browser or WebView.
+
+### Screenshots
+
+![Text with RubySpan](screenshots/1.png)
+
+### Known Bugs
+
+1. Line breaking doesn't work properly on Android pre 5.0. It's painful to solve this.
+
+2. This is actually not a bug. Since RubySpan extends ReplacementSpan, it does not compatible with other (mostly) spans when used on the same text. You have to modify the source code if you want to add more features.
+
+3. The first character of a line will not be rendered properly if it needs to be indented to display its long annotation. AFAIK, this is technically unsolvable.
+
+4. [ReplacementSpan's bug](https://code.google.com/p/android/issues/detail?id=209437)
+
+### License
+
+Copyright (c) 2016 Minglangjun Li  
+Licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
---
+++ app/src/main/java/io/github/mljli/rubyspan/MainActivity.java
@@ -0,0 +1,54 @@
+package io.github.mljli.rubyspan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String sampleText = "<p><span class='colorL'>カンボジア</span>の<ruby>経済<rt>けいざい</rt></ruby>は、<ruby>去年<rt>きょねん</rt></ruby>までの５<ruby>年<rt>ねん</rt></ruby>、７％<ruby>以上<rt>いじょう</rt></ruby>の<a href='javascript:void(0)' class='dicWin' id='id-0001'><ruby><span class=\"under\">成長</span><rt>せいちょう</rt></ruby></a>を<ruby>続<rt>つづ</rt></ruby>けていて、<span class='colorL'><ruby>日本<rt>にっぽん</rt></ruby></span>の<ruby>会社<rt>かいしゃ</rt></ruby>が<ruby>毎年<rt>まいとし</rt></ruby>２００ぐらい<ruby>増<rt>ふ</rt></ruby>えています。<span class='colorL'>プノンペン</span>では<ruby>日本料理<rt>にほんりょうり</rt></ruby>のレストランも<ruby>増<rt>ふ</rt></ruby>えています。<span class='colorL'><ruby>日本<rt>にっぽん</rt></ruby></span>と<span class='colorL'>カンボジア</span>の<ruby>間<rt>あいだ</rt></ruby>を<ruby>飛<rt>と</rt></ruby>ぶ<ruby>飛行機<rt>ひこうき</rt></ruby>で、<span class='colorL'><ruby>日本<rt>にっぽん</rt></ruby></span>から<ruby>野菜<rt>やさい</rt></ruby>や<ruby>肉<rt>にく</rt></ruby>などを<ruby>運<rt>はこ</rt></ruby>びやすくなると<ruby>考<rt>かんが</rt></ruby>えられています。</p>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textview);
+        textView.setTextSize(28);
+        // IMPORTANT!
+        textView.setLineSpacing(0, 1.5f);
+        float textSize = textView.getTextSize();
+        textView.setPadding(0, (int)(textSize/2 + 5), 0, 0);
+
+        SpannableStringBuilder ssb = new SpannableStringBuilder("東京へ行きたい");
+        ssb.setSpan(new RubySpan("とうきょう"), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(new RubySpan("い"), 3, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ssb);
+
+
+
+        TextView textView2 = (TextView)findViewById(R.id.textview2);
+        textView2.setTextSize(28);
+        // IMPORTANT!
+        textView2.setLineSpacing(0, 1.5f);
+        textView2.setPadding(0, (int)(textSize/2 + 5), 0, 0);
+
+        ssb = new SpannableStringBuilder("あ飛行機車あ");
+        ssb.setSpan(new RubySpan("ひこうき"), 1, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(new RubySpan("くるま"), 4, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView2.setText(ssb);
+
+
+
+        TextView textView3 = (TextView)findViewById(R.id.textview3);
+        textView3.setTextSize(20);
+        // IMPORTANT!
+        textView3.setLineSpacing(0, 1.5f);
+        textSize = textView3.getTextSize();
+        textView3.setPadding(0, (int)(textSize/2 + 5), 0, 0);
+        textView3.setText(Html.fromHtml(sampleText, null, new RubyTagHandler()));
+    }
+}
